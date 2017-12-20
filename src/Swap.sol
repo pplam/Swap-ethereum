9e26c92e3df328af3f694bfc1de6b54113461901
Luo Shiwen <shiwen.l.ating@gmail.com>
2017-12-20 18:31:24 +0800

    not use ds-auth

--- src/Swap.sol
+++ src/Swap.sol
@@ -1,17 +1,49 @@
 pragma solidity ^0.4.19;
 
-import "ds-auth/auth.sol"
+import "erc20/erc20.sol";
 
-contract WhiteListAuth is DSAuthority {
-  mapping (address => bool) whiteList;
+contract Swap {
+  ERC20 public ATN;
 
-  function canCall(
-      address src, address dst, bytes4 sig
-  ) public constant returns (bool) {
-    return whiteList[src]
+  address public owner;
+  mapping (address => bool) public whiteList;
+
+  uint public requiredProofNumber;
+  mapping (string => uint) public proofList;
+
+  function Swap(address _atn) {
+    owner = msg.sender;
+    ATN = ERC20(_atn);
   }
-}
 
-contract Swap is DSAuth {
-  function Swap() {}
+  function setOwner(address owner_)
+    public
+    onlyOwner
+  {
+    owner = owner_;
+  }
+
+  function permit(address addr)
+    public
+    onlyOwner
+  {
+    whiteList[addr] = true;
+  }
+
+  function forbid(address addr)
+    public
+    onlyOwner
+  {
+    whiteList[addr] = false;
+  }
+
+  modifier onlyOwner {
+    require(msg.sender == owner);
+    _;
+  }
+
+  modifier authorized {
+    require(msg.sender == owner || whiteList[msg.sender]);
+    _;
+  }
 }
