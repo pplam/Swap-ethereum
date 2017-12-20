4d7e7b0526605031d1cf39c690bc4a9f25244b3c
Luo Shiwen <shiwen.l.ating@gmail.com>
2017-12-20 17:42:31 +0800

    create Authority

--- src/Swap.sol
+++ src/Swap.sol
@@ -1,6 +1,17 @@
 pragma solidity ^0.4.19;
 
+import "ds-auth/auth.sol"
 
-contract Swap {
-  function Swap
+contract WhiteListAuth is DSAuthority {
+  mapping (address => bool) whiteList;
+
+  function canCall(
+      address src, address dst, bytes4 sig
+  ) public constant returns (bool) {
+    return whiteList[src]
+  }
+}
+
+contract Swap is DSAuth {
+  function Swap() {}
 }
